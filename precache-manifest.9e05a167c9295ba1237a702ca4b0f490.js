b7b7ab4a641735e33a37f78a433d508857ea48e7
Your Name <you@example.com>
2019-04-29 13:16:30 +0800

    initial commit

---
+++ precache-manifest.9e05a167c9295ba1237a702ca4b0f490.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dfc3709e42d342fb2c44",
+    "url": "/js/chunk-vendors.4a71c6a6.js"
+  },
+  {
+    "revision": "b471b07f42dc6d64ec3a",
+    "url": "/js/app.c5add572.js"
+  },
+  {
+    "revision": "fb76da48c4245953e8bde4ad6f957a02",
+    "url": "/index.html"
+  }
+];
